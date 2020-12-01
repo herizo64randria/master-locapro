f3d53422c2985583fbed9c0dbe5700b9950b06d6
Allin RAMANAMPISOA <Aallin13ramanampisoa@outlook.fr>
2020-12-01 11:06:36 +0300

    Récap. statistique - avec chois mois

--- src/GroupeBundle/Controller/StatistiqueController.php
+++ src/GroupeBundle/Controller/StatistiqueController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 /**
  * Site controller.
@@ -65,7 +66,7 @@ class StatistiqueController extends Controller
                     $huileAppoint += $appoint->getHuileUtilise();
             }
 
-            $ligne['huileAppoint'] = $huileVidange;
+            $ligne['huileAppoint'] = $huileAppoint;
 
             // ------------------///// APPOINT /////------------------
 
@@ -85,7 +86,73 @@ class StatistiqueController extends Controller
      */
     public function index_byDate_AppointVidangeAction(Request $request)
     {
-        
+        if($request->getMethod() == 'POST'){
+            $em = $this->getDoctrine()->getManager();
+
+            $mois = (int)$_POST['moisStat'];
+            $annee = (int)$_POST['anneeStat'];
+
+            $_dateDebut = date("d/m/Y", mktime(0, 0, 0, $mois, 1 ,$annee));
+            $_dateFin = date("d/m/Y", mktime(0, 0, 0, $mois +1, 0, $annee));
+
+            $dateDebut = \DateTime::createFromFormat('d/m/Y', $_dateDebut);
+            $dateDebut->setTime(0,0);
+
+            $dateFin = \DateTime::createFromFormat('d/m/Y', $_dateFin);
+            $dateFin->setTime(23,59, 59);
+
+            $groupes = $em->getRepository('GroupeBundle:Groupe')->findAll();
+
+            $repositoryVidange = $em->getRepository('GroupeBundle:Vidange');
+            $repositoryAppoint = $em->getRepository('GroupeBundle:Appoint');
+
+            $tableauListes = array();
+            foreach ($groupes as $groupe){
+
+                //----- L'HEURE DE MARCHE EST DEJA DANS LE GROUPE -----
+                $ligne['groupe'] = $groupe;
+
+                // ------------------ VIDANGE ------------------
+
+                $huileVidange = 0;
+
+                $vidanges = $repositoryVidange->findByDateAndGroupe($groupe, $dateDebut, $dateFin);
+
+                foreach ($vidanges as $vidange){
+                    if($vidange->getHuileUtilise())
+                        $huileVidange += $vidange->getHuileUtilise();
+                }
+
+                $ligne['huileVidange'] = $huileVidange;
+
+                // ------------------///// VIDANGE /////------------------
+
+                // ------------------ APPOINT ------------------
+
+                $huileAppoint = 0;
+
+                $appoints = $repositoryAppoint->findByDateAndGroupe($groupe, $dateDebut, $dateFin);
+
+                foreach ($appoints as $appoint){
+                    if($appoint->getHuileUtilise())
+                        $huileAppoint += $appoint->getHuileUtilise();
+                }
+
+                $ligne['huileAppoint'] = $huileAppoint;
+
+                // ------------------///// APPOINT /////------------------
+
+                array_push($tableauListes, $ligne);
+            }
+
+            return $this->render('@Groupe/statistique/indexByDateVidaneAppoint.html.twig', array(
+                'tables' => $tableauListes,
+                'dateDebut' => $dateDebut
+            ));
+
+
+        }else
+           throw new Exception('Erreur 404 NOT-FOUND');
     }
 
 }
